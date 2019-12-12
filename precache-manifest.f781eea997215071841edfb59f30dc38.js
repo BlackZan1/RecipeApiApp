b673d97f4b5a962370fa3ba5ebfd3b9d526815f5
BlackZan1 <kenshin.nakamura.01@mail.ru>
2019-12-12 17:05:06 +0300

    Updates

---
+++ precache-manifest.f781eea997215071841edfb59f30dc38.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e8e6be817d25b2152d73dda07876ae86",
+    "url": "/RecipeApiApp/index.html"
+  },
+  {
+    "revision": "7152961ec0965fa566ff",
+    "url": "/RecipeApiApp/static/css/main.b7be97d9.chunk.css"
+  },
+  {
+    "revision": "950e6ad12ecbae9fe33e",
+    "url": "/RecipeApiApp/static/js/2.92aa4367.chunk.js"
+  },
+  {
+    "revision": "83b3d04082a8c50f224f3e213e6a5a45",
+    "url": "/RecipeApiApp/static/js/2.92aa4367.chunk.js.LICENSE"
+  },
+  {
+    "revision": "7152961ec0965fa566ff",
+    "url": "/RecipeApiApp/static/js/main.fda30f6f.chunk.js"
+  },
+  {
+    "revision": "b5eb83b442a2178f2d71",
+    "url": "/RecipeApiApp/static/js/runtime-main.832f3cdf.js"
+  }
+]);
